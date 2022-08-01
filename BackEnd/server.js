fda24640b6df60ac33f77195ce7047e1090b8588
Your Name <fedi.benslama@esprit.tn>
2022-08-01 04:51:02 +0100

    Starting implement Routes

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -21,55 +21,7 @@ db = knex({
 app.use(cors());
 app.use(express.json());
 
-// const database = {
-//     projects: [
-//         {
-//             id: "1",
-//             Name: "Project1",
-//             Type: "Web Developement",
-//             UsedSolutions: "React,JavaScript,NodeJS,MySQL",
-//             AssociatedServers: "Development Server",
-//             AssociatedClient: "Client1",
-//             Status: "Under Development"
-//         },
-//         {
-//             id: "2",
-//             Name: "Project2",
-//             Type: "Mobile Application",
-//             UsedSolutions: "Flutter,React",
-//             AssociatedServers: "Development Server",
-//             AssociatedClient: "Client2",
-//             Status: "Under Development"
-//         },
-//         {
-//             id: "3",
-//             Name: "Project3",
-//             Type: "Data Science",
-//             UsedSolutions: "Machine Learning,Deep Learning,Python",
-//             AssociatedServers: "Production Server",
-//             AssociatedClient: "Client3",
-//             Status: "Testing"
-//         },
-//         {
-//             id: "4",
-//             Name: "Project4",
-//             Type: "Software Development",
-//             UsedSolutions: "Java,Python",
-//             AssociatedServers: "Test Server",
-//             AssociatedClient: "Client4",
-//             Status: "Validated"
-//         },
-//         {
-//             id: "5",
-//             Name: "Project5",
-//             Type: "Cloud Computing",
-//             UsedSolutions: "Machine Learning,AWS",
-//             AssociatedServers: "Development Server",
-//             AssociatedClient: "Client5",
-//             Status: "Testing"
-//         }
-//     ]
-// }
+
 
 // -------------Get All Projects-------------------
 app.get('/projects', (req, res) => {
@@ -80,23 +32,23 @@ app.get('/projects', (req, res) => {
 })
 
 // -------------Get one Project-------------------
-app.get('/project/:id', (req, res) => {
-    const { id } = req.params;
-    let found = false;
-    database.projects.forEach(project => {
-        if (project.id === id) {
-            found = true;
-            return res.json(project)
-        }
-    })
-    if (!found) {
-        res.status(400).json('Project Not Found')
-    }
+app.get('/ViewProject/:id', (req, res) => {
+    
+        const { id } = req.params;
+        
+        db.select('*').from('projects').where({ id: id })
+            .then(response => {
+                if (response.length) {            //To Check if project exists , we can change 'response' to 'project' for clarity
+                    res.json(response[0]);
+                } else { res.status(400).json('Project Not Found') }
+    
+            })
+            .catch(err => res.status(400).json('Project Not Found'))
 })
 
 // -------------Add one Project-------------------
 app.post('/addProject', (req, res) => {
-    const { Name, Type, UsedSolutions, AssociatedServers, AssociatedClient, Status } = req.body; //to avoid repeating req.body
+    const { Name, Type, UsedSolutions, AssociatedServers, AssociatedClient, Status, ProjectProgress, StartDate, FinishDate, ProjectDescription } = req.body; //to avoid repeating req.body
     db('projects')
         .returning('*')  //our response is all the columns
         .insert({
@@ -107,6 +59,10 @@ app.post('/addProject', (req, res) => {
             associatedservers: AssociatedServers,
             associatedclient: AssociatedClient,
             status: Status,
+            projectprogress: ProjectProgress,
+            startdate: StartDate,
+            finishdate: FinishDate,
+            projectdescription: ProjectDescription
         }).then(project => {
             res.json(project[0]);
         })
@@ -115,23 +71,13 @@ app.post('/addProject', (req, res) => {
         })
 
 
-    // database.projects.push({
-    //     id: id,
-    //     Name: Name,
-    //     Type: Type,
-    //     UsedSolutions: UsedSolutions,
-    //     AssociatedServers: AssociatedServers,
-    //     AssociatedClient: AssociatedClient,
-    //     Status: Status,
-    // })
-    // res.json(database.projects[database.projects.length - 1]);
 
 })
 // ------------------ Edit A Project ------------------
 app.put('/EditProject', (req, res) => {
-    
 
-    const {id, Name, Type, UsedSolutions, AssociatedServers, AssociatedClient, Status } = req.body; //to avoid repeating req.body
+
+    const { id, Name, Type, UsedSolutions, AssociatedServers, AssociatedClient, Status, ProjectProgress, StartDate, FinishDate, ProjectDescription } = req.body; //to avoid repeating req.body
     db('projects')
         .returning('*')  //our response is all the columns
         .where('id', '=', id)
@@ -143,6 +89,10 @@ app.put('/EditProject', (req, res) => {
             associatedservers: AssociatedServers,
             associatedclient: AssociatedClient,
             status: Status,
+            projectprogress: ProjectProgress,
+            startdate: StartDate,
+            finishdate: FinishDate,
+            projectdescription: ProjectDescription
         }).then(project => {
             res.json(project[0]);
         })
@@ -151,40 +101,9 @@ app.put('/EditProject', (req, res) => {
         })
 
 
-    // database.projects.push({
-    //     id: id,
-    //     Name: Name,
-    //     Type: Type,
-    //     UsedSolutions: UsedSolutions,
-    //     AssociatedServers: AssociatedServers,
-    //     AssociatedClient: AssociatedClient,
-    //     Status: Status,
-    // })
-    // res.json(database.projects[database.projects.length - 1]);
 
 })
-    // let found = false;
-    // const { id, Name, Type, UsedSolutions, AssociatedServers, AssociatedClient, Status } = req.body
-    // database.projects.forEach(project => {
-    //     if (project.id === id) {
-    //         found = true;
-    //         const index = database.projects.findIndex((project) => project.id === id);
-    //         database.projects[index] = {
-    //             Name: Name,
-    //             Type: Type,
-    //             UsedSolutions: UsedSolutions,
-    //             AssociatedServers: AssociatedServers,
-    //             AssociatedClient: AssociatedClient,
-    //             Status: Status
-    //         }
 
-    //         return res.json(database.projects[index]);
-    //     }
-
-    // })
-    // if (!found) {
-    //     res.status(404).json("Project Not Found")
-    // }
 
 // ------------------ Delete A Project ------------------
 app.put('/DeleteProject', (req, res) => {
@@ -198,19 +117,7 @@ app.put('/DeleteProject', (req, res) => {
             res.json("Deleted")
         })
         .catch(err => res.status(400).json('Unable To Delete'))
-    // let found = false;
-    // database.projects.forEach(project => {
-    //     if (project.id === id) {
-    //         found = true;
-    //         const index = database.projects.findIndex((project) => project.id === id);
-    //         database.projects.splice(index, 1)
-    //         return res.json(database.projects);
-    //     }
 
-    // })
-    // if (!found) {
-    //     res.status(404).json("Project Not Found")
-    // }
 })
 
 
@@ -220,9 +127,3 @@ app.listen(3001, () => {
     console.log('App Is Running On Port 3001')
 })
 
-/*
-/ --> res = this is working
-/add --> POST = project
-/edit -->Put = Project
-/view/:projectID --> Get = Project
-/delete --> Put Project */
